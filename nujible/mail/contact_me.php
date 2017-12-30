15bdbfa169782dd0e5621d2db6722811f27122ad
info1010 <34713942+info1010@users.noreply.github.com>
2017-12-30 16:16:30 +0530

    Update contact_me.php

--- nujible/mail/contact_me.php
+++ nujible/mail/contact_me.php
@@ -18,8 +18,8 @@ $to = $email_address; // Add your email address inbetween the '' replacing yourn
 $email_subject = "Website Contact Form:  $name";
 $email_body = "Thank you for contact us , We will get back to you soon...\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
 $headers = "From: noreply@infoenum.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
+$headers .= "Reply-To: noreply@infoenum.com";	
 //mail($to,$email_subject,$email_body,$headers);
-mail($to,"aakash.dhingrolia@infocnum.com",$email_body,$headers);
+mail($to,"noreply@github.com",$email_body,$headers);
 return true;			
 ?>
